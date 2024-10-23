e5acbc897758c41dd1d362a7e227766333b86dfb
mateuskrause <mateus.ke@outlook.com>
2024-10-22 23:16:55 -0300

    script to download online polyhedron files

---
+++ get_poly.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# This script is to download polyhedron files for educational purposes only. it is intended for use in the polyh-view visualization tool.
+# files are available under Copyright © 2015 David I. McCooey. All Rights Reserved (http://dmccooey.com/polyhedra/)
+# all the files are under the terms of the copyright owner. do not distribute without permission!
+
+wget -P "./polyhedra/" \
+http://dmccooey.com/polyhedra/Tetrahedron.txt \
+http://dmccooey.com/polyhedra/Octahedron.txt \
+http://dmccooey.com/polyhedra/Cube.txt \
+http://dmccooey.com/polyhedra/Icosahedron.txt \
+http://dmccooey.com/polyhedra/Dodecahedron.txt \
+http://dmccooey.com/polyhedra/SmallStellatedDodecahedron.txt \
+http://dmccooey.com/polyhedra/Rhombicuboctahedron.txt \
+http://dmccooey.com/polyhedra/TruncatedCube.txt \
+http://dmccooey.com/polyhedra/TruncatedIcosidodecahedron.txt \
+http://dmccooey.com/polyhedra/SmallDodecicosacron.txt \
+http://dmccooey.com/polyhedra/GreatDodecicosahedron.txt \
+http://dmccooey.com/polyhedra/TriakisTetrahedron.txt \
+http://dmccooey.com/polyhedra/MedialInvertedPentagonalHexecontahedron.txt \
+http://dmccooey.com/polyhedra/OctagonalAntiprism.txt \
+http://dmccooey.com/polyhedra/ExpandedJoinedHexpropelloDodecahedron.txt \
+http://dmccooey.com/polyhedra/KleinDual.txt \
+http://dmccooey.com/polyhedra/Schonhardt.txt \
+http://dmccooey.com/polyhedra/RhombicIcosahedron.txt
